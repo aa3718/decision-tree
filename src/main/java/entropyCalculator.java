301725ee38e00870d4d7d1f62d19d4ea91b95d76
aa3718 <aa3718@ic.ac.uk>
2020-04-18 18:21:44 +0200

    Commit

--- src/main/java/entropyCalculator.java
+++ src/main/java/entropyCalculator.java
@@ -3,6 +3,6 @@ import java.lang.Math;
 public class entropyCalculator {
 
     public static void main(String args[]) {
-       System.out.println("Hey first commit!");
+        System.out.println("Hey first commit!");
     }
 }
